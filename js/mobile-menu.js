c6d8df8ad427e72b9bb82b6aba172998d527c5dc
Kateryna Tolstykh <agrratt@gmail.com>
2021-09-30 23:53:06 +0300

    Добавляет работающий скрипт для моб меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,11 +1,14 @@
 (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
-  const mobileBtnClose = document.querySelector('[data-menu-close]');
-  menuBtnRef.addEventListener('click', () => {
-    mobileMenuRef.classList.toggle('is-open');
-  });
-  mobileBtnClose.addEventListener('click', () => {
-    mobileMenuRef.classList.toggle('is-open');
-  });
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
 })();
